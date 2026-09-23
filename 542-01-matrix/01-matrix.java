7af328de0da81ba5f63422f5643dc6227f3e6935
Smrutishree <smruti0303@gmail.com>
2026-09-23 23:21:59 +0530

    Time: 13 ms (91.52%) | Memory: 48.7 MB (87.01%) - LeetSync

---
+++ 542-01-matrix/01-matrix.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+
+        // Add all 0s to the queue
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    q.offer(new int[]{i, j});
+                } else {
+                    mat[i][j] = -1; // unvisited
+                }
+            }
+        }
+
+        int[][] directions = {
+            {1, 0}, {-1, 0},
+            {0, 1}, {0, -1}
+        };
+
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            int r = curr[0];
+            int c = curr[1];
+
+            for (int[] dir : directions) {
+                int nr = r + dir[0];
+                int nc = c + dir[1];
+
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n
+                        && mat[nr][nc] == -1) {
+
+                    mat[nr][nc] = mat[r][c] + 1;
+                    q.offer(new int[]{nr, nc});
+                }
+            }
+        }
+
+        return mat;
+    }
+}
